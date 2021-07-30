88d67fb750f1232c114427a21165422c007181c7
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2021-07-30 13:54:42 +0600

    ⚒  Refactored the SearchBar component to make it a Class Component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,42 +1,64 @@
 import PropTypes from "prop-types";
 
-import { useEffect, useRef, useState } from "react";
+import { Component } from "react";
 import { Wrapper, Content } from "./SearchBar.styles";
 import SearchIcon from "../images/search-icon.svg";
 
-const SearchBar = ({ setSearchTerm }) => {
-  const [state, setState] = useState("");
-  const initial = useRef(true);
+class SearchBar extends Component {
+  // const [state, setState] = useState("");
+  // const initial = useRef(true);
 
-  useEffect(() => {
-    if (initial.current) {
-      initial.current = false;
-      return;
+  // useEffect(() => {
+  //   if (initial.current) {
+  //     initial.current = false;
+  //     return;
+  //   }
+
+  //   const timer = setTimeout(() => {
+  //     setSearchTerm(state);
+  //   }, 1000);
+
+  //   return () => clearImmediate(timer);
+  // }, [setSearchTerm, state]);
+
+  state = { value: "" };
+  timeout = null;
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (this.state.value !== prevState.value) {
+      const { setSearchTerm } = this.props;
+
+      clearTimeout(this.timeout);
+
+      this.timeout = setTimeout(() => {
+        const { value } = this.state;
+        setSearchTerm(value);
+      }, 1000);
     }
+  }
 
-    const timer = setTimeout(() => {
-      setSearchTerm(state);
-    }, 1000);
+  render() {
+    const { value } = this.state;
 
-    return () => clearImmediate(timer);
-  }, [setSearchTerm, state]);
-
-  return (
-    <Wrapper>
-      <Content>
-        <img src={SearchIcon} alt="search-icon" />
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search Movie"
-          onChange={(event) => setState(event.currentTarget.value)}
-          value={state}
-        />
-      </Content>
-    </Wrapper>
-  );
-};
+    return (
+      <Wrapper>
+        <Content>
+          <img src={SearchIcon} alt="search-icon" />
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search Movie"
+            onChange={(event) =>
+              this.setState({ value: event.currentTarget.value })
+            }
+            value={value}
+          />
+        </Content>
+      </Wrapper>
+    );
+  }
+}
 
 SearchBar.propTypes = {
   setSearchTerm: PropTypes.func,
